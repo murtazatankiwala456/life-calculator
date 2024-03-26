09a204fa0148f46f515c0c2ea42c5b7469426f68
Murtaza Tankiwala <115138620+murtazatankiwala456@users.noreply.github.com>
2024-03-26 13:05:07 +0530

    commit 2
    variable change

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let btn = document.querySelector(".btn"); // grab the button dom
+const btn = document.querySelector(".btn"); // grab the button dom
 
 btn.addEventListener("click", calculateAge); // add eventListener to button when got clicked
 
@@ -6,10 +6,10 @@ btn.addEventListener("click", calculateAge); // add eventListener to button when
 function calculateAge() {
   let input = document.querySelector("#input").value; // grab the input field value
   let result = document.querySelector(".result"); // grab the div to append output
-  let ageLeft = 100 - input; // subtracts input field value with 100 to get  age left
-  let days = ageLeft * 365; // multiply it by number of days in a year
-  let weeks = ageLeft * 52; // mutiply it by number of weeks in a year
-  let months = ageLeft * 12; // mutiply it by number of months in a year
+  const ageLeft = 100 - input; // subtracts input field value with 100 to get  age left
+  const days = ageLeft * 365; // multiply it by number of days in a year
+  const weeks = ageLeft * 52; // mutiply it by number of weeks in a year
+  const months = ageLeft * 12; // mutiply it by number of months in a year
   if (isNaN(input)) {
     result.innerHTML = `<div class= 'alert alert-danger'>Please Enter a number!</div>`;
   } else if (input === "") {
